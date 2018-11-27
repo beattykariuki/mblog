b29b4e5fc0fa548a42aaf3a7e87afea99444575c
beattykariuki <bettykariuki026@gmail.com>
2018-11-27 11:55:37 +0300

    added test

--- tests/test_cases.py
+++ tests/test_cases.py
@@ -28,41 +28,4 @@ class PostTest(unittest.TestCase):
                              subtitle='subtitle',
                              content="content")
 
-    def tearDown(self):
-        Post.query.delete()
-
-    def test_init(self):
-        self.assertEquals(self.new_post.id, 1)
-        self.assertEquals(self.new_post.title, "title")
-        self.assertEquals(self.new_post.subtitle, "subtitle")
-        self.assertEquals(self.new_post.content, "content")
-
-
-class CommentTest(unittest.TestCase):
-    def setUp(self):
-        self.new_comment = Comment(1,"comment",1,1)
-
-    def test_instance(self):
-        self.assertTrue(isinstance(self.new_comment,Comment))
-
-    def test_init(self):
-        self.assertEqual(self.new_comment.id, 1)
-        self.assertEqual(self.new_comment.comment, "comment")
-        self.assertEqual(self.new_comment.user_id, 1)
-        self.assertEqual(self.new_comment.post_id, 1)
-
-    def test_save_comment(self):
-        self.new_comment.save_comment()
-        self.assertTrue(len(Comment.query.all()) > 0)
-
-
-    def test_get_comment_by_post_id(self):
-        self.new_comment.save_comment()
-        get_comments = Comment.get_comments(12345)
-        self.assertTrue(len(get_comments) == 1)
-
-
-
-
-
-
+    
